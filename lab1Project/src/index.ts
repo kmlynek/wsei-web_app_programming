5d50cc8a5635713858350304460a218a1c9c6185
Karol <kmlynek65@gmail.com>
2021-03-07 20:56:39 +0100

    task

---
+++ lab1Project/src/index.ts
@@ -0,0 +1,11 @@
+class StatsApp{
+    data1Input: HTMLInputElement;
+    data2Input: HTMLInputElement;
+    data3Input: HTMLInputElement;
+    data4Input: HTMLInputElement;
+    sumInput: HTMLInputElement;
+    avgInput: HTMLInputElement;
+    minInput: HTMLInputElement;
+    maxInput: HTMLInputElement;
+    }
+const statsApp = new StatsApp();
